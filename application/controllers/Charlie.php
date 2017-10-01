7cfa46af9b9d6cde4c2df7df19ad8146d67cb9e4
sbukharov <sergey@bukharov.info>
2017-10-01 15:11:01 -0700

    Added Charlie route. Closes #4.

---
+++ application/controllers/Charlie.php
@@ -0,0 +1,26 @@
+<?php
+
+class Charlie extends Application
+{
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Homepage for our app
+	 */
+	public function index()
+	{
+            // this is the view we want shown
+            //$this->show(1);
+	}
+        
+        public function brown()
+        {
+                // this is the view we want shown
+                $this->show(3);
+        }
+
+}
